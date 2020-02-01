3129dfd6873e3253290e1882761d67a2a5d8c85f
Daniel Kwong <dkwon014@ucr.edu>
2020-02-01 03:33:56 -0800

    yes

--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -90,18 +90,7 @@ export class LandingPage extends Component {
                          playing="true"
                        />
                        <p>
-                         Lorem Ipsum is simply dummy text of the printing and
-                         typesetting industry. Lorem Ipsum has been the
-                         industry's standard dummy text ever since the 1500s,
-                         when an unknown printer took a galley of type and
-                         scrambled it to make a type specimen book. It has
-                         survived not only five centuries, but also the leap
-                         into electronic typesetting, remaining essentially
-                         unchanged. It was popularised in the 1960s with the
-                         release of Letraset sheets containing Lorem Ipsum
-                         passages, and more recently with desktop publishing
-                         software like Aldus PageMaker including versions of
-                         Lorem Ipsum.
+                         Welcome to EducateMe - the first free and live education platform for instructors and students.
                        </p>
                      </center>
                      <MDBBtn onClick={this.logout}>Logout</MDBBtn>
