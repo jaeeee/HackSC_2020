dd544b69a4961da69c03e769c27eca4c0f6cae34
Daniel Kwong <dkwon014@ucr.edu>
2020-02-01 02:18:36 -0800

    Guide comp added

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -18,32 +18,35 @@ export class NavBar extends Component {
     render() {
         return (
           <div>
-        {/* // <div> */}
-             <MDBNavbar color="indigo" dark expand="md">
-          <MDBNavbarBrand>
-            <strong className="white-text">EducateMe</strong>
-          </MDBNavbarBrand>
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
-          {/* <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar> */}
-          <MDBNavbarNav left>
-            <MDBNavItem active>
-              <MDBNavLink to="../">Home</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              {/* <Link to="/streams">About Me</Link> */}
+            {/* // <div> */}
+            <MDBNavbar color="indigo" dark expand="md">
+              <MDBNavbarBrand>
+                <strong className="white-text">EducateMe</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.toggleCollapse} />
+              {/* <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar> */}
+              <MDBNavbarNav left>
+                <MDBNavItem active>
+                  <MDBNavLink to="../">Home</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  {/* <Link to="/streams">About Me</Link> */}
 
-              <MDBNavLink to={"/streams"}>Streams</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="/categories">Categories</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="/profile">Profile</MDBNavLink>
-            </MDBNavItem>
-          </MDBNavbarNav>
-          {/* </div> */}
-          {/* </div> */}
-          </MDBNavbar>
+                  <MDBNavLink to={"/streams"}>Streams</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/categories">Categories</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/profile">Profile</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/guide">Guide</MDBNavLink>
+                </MDBNavItem>
+              </MDBNavbarNav>
+              {/* </div> */}
+              {/* </div> */}
+            </MDBNavbar>
           </div>
         );
     }
--- app/src/components/main.js
+++ app/src/components/main.js
@@ -5,6 +5,7 @@ import LandingPage from "./LandingPage.js";
 import CurrentStreams from "./CurrentStreams.js";
 import Profile from './Profile';
 import Categories from './Categories';
+import Guide from "./Guide.js";
 // import AboutMe from "./aboutme";
 // import Contact from "./contact";
 // import Projects from "./projects";
@@ -16,6 +17,7 @@ const Main = () => (
     <Route path="/streams" component={CurrentStreams}/>
     <Route path="/profile" component={Profile}/>
     <Route path="/categories" component={Categories}/>
+    <Route path="/guide" component={Guide}/>
     {/* <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
