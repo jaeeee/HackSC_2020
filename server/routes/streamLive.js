d0100b5a73b0d43ed3202def7edc003ca06cc98d
Daniel Kwong <dkwon014@ucr.edu>
2020-02-01 12:47:33 -0800

    Create stream lmao

--- server/routes/streamLive.js
+++ server/routes/streamLive.js
@@ -1,16 +1,29 @@
 var express = require("express");
 var app = express.Router();
-const Mux = require('@mux/mux-node');
+const Mux = require("@mux/mux-node");
+const MUX_TOKEN_ID = "7d5c07fb-dab4-4784-b353-b4b337c39355";
+const MUX_TOKEN_SECRET =
+  "GWvsq958vsvkWwZr6uySoILhfKH6AoVP/Ti8spZ7b9+RqNvhpt9UsSAmkFEqTefoE9IJmwmF3id";
+const { Video, Data } = new Mux(MUX_TOKEN_ID, MUX_TOKEN_SECRET);
 
-const  {Video, Data} = new Mux();
+// const asset  = await Vide.Assets.create({
+//   input: "",
+// });
 
-const asset  = await Vide.Assets.create({
-  input: "",
+app.post("/", function(req, res, next) {
+  console.log("suh dude");
+  Video.LiveStreams.create({
+    playback_policy: "public",
+    new_asset_settings: { playback_policy: "public" }
+  })
+    .then(muxResponse => {
+      console.log("muxResponse", muxResponse);
+      res.json({
+        playbackid: muxResponse.playback_ids[0].id,
+        stream_key: muxResponse.stream_key
+      });
+    })
+    .catch(next);
 });
-
-app.get("/", function(req, res, next) {
-    res.send("fDNFIAFMDI");
-});
-
 
 module.exports = app;
