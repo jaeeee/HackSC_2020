d3bfabfc05931f36e6b7a796e9acf5ab2a4eb5c8
Rahul Nair <rnair12599@gmail.com>
2020-02-02 00:18:52 -0800

    yessir

--- app/src/components/Chat.js
+++ app/src/components/Chat.js
@@ -13,20 +13,77 @@ import "stream-chat-react/dist/css/index.css";
 const chatClient = new StreamChat("4zbz7yw374h6");
 const userToken =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGl2aW5lLWFydC05In0.0Ce-vmn7fcec-PCD2OWy3sIVRLrmKRhBXp69vSGlgtw";
+let localChatToken = "";
+let localChatname = "";
+let localChatID = "";
+let localChatImage = "";
+
+
 
 chatClient.setUser(
   {
-    id: "divine-art-9",
-    name: "Divine art",
-    image: "https://getstream.io/random_svg/?id=divine-art-9&name=Divine+art"
+    id: localChatID,
+    name: localChatname,
+    image: localChatImage
   },
-  userToken
+  localChatToken
 );
 
 const channel = chatClient.channel("livestream", "spacex", {
   image: "https://goo.gl/Zefkbx",
   name: "SpaceX launch discussion"
 });
+
+
+componentWillUnmount() {
+  this._isMounted = false;
+}
+
+componentDidMount() {
+  this._isMounted = true;
+
+  let currentComp = this;
+
+  fire.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var found = 0;
+      var userRef = db.collection("users").doc(fire.auth().currentUser.email);
+      var username = fire.auth().currentUser.email;
+      userRef.onSnapshot(
+        {
+          includeMetadataChanges: true
+        },
+        function(doc) {
+          try {
+
+
+              localChatID = doc.data().chatID,
+              localChatImage = doc.data().chatImage,
+              localChatname =  doc.data().chatName,
+              localChatToken = doc.data().chatToken,
+
+
+          } catch (error) {
+            console.log("We getting an error: ", error);
+            found = 0;
+          }
+        }
+      );
+
+      if (found == 1) {
+        //console.log("does hit this")
+      } else {
+        currentComp.setState({
+          budget: ["N/A"]
+        });
+      }
+    }
+  });
+};
+
+
+
+
 
 const Chatter = () => (
   <Chat client={chatClient} theme={"livestream dark"}>
